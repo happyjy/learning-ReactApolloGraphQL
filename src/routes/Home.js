0e7e98984a7553e123c080b80ff7e862245150a3
happyjy <okwoyjy@gmail.com>
2021-01-02 12:35:50 +0900

    GET ytsMovies Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,23 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  query {
+    ytsMovies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) {
+    return 'loading...';
+  }
+
+  if (data && data.ytsMovies) {
+    return data.ytsMovies.map((m) => <h1>{m.id}</h1>);
+  }
+};
