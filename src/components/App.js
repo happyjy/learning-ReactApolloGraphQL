08dddd24e21cfc41f70eb0f4590ec4e5dd9711a5
happyjy <okwoyjy@gmail.com>
2021-01-02 11:28:49 +0900

    초기화

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
